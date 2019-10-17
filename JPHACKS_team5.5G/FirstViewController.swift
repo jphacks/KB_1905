a1a1774287ae37ba472a7fc28e767e2e2b3be5ee
AkihiroKanda <akanda@mikilab.doshisha.ac.jp>
2019-10-17 16:45:21 +0900

    Initial Commit

---
+++ JPHACKS_team5.5G/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  JPHACKS_team5.5G
+//
+//  Created by 神田章博 on 2019/10/17.
+//  Copyright © 2019 KandaAkihiro. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ JPHACKS_team5.5G/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  JPHACKS_team5.5G
+//
+//  Created by 神田章博 on 2019/10/17.
+//  Copyright © 2019 KandaAkihiro. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
