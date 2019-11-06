79bd28a519196d60411af3365bca6aa53ff3d2d8
Ubuntu <ubuntu@ip-172-31-41-166.ap-northeast-1.compute.internal>
2019-11-06 06:57:05 +0000

    履歴表示にmoveの判定を追加

--- server/routes/sensors.js
+++ server/routes/sensors.js
@@ -130,8 +130,37 @@ router.post('/rssi', function(req, res) {
   res.send('ok');
 });
 
-// 履歴表示用
-router.get('/moveHistory', function(req, res, next) {
+// 履歴表示用(前後30秒)
+// router.get('/moveHistory', function(req, res, next) {
+//   let start_time = Number(req.query.timestamp);
+
+//   let params = {
+//     TableName : 'sensors'
+//   };
+
+//   docClient.scan(params, function(err, data) {
+//     if (err){
+//       console.log(err);
+//       res.send(err);
+//     } else {
+//       let items = data.Items;
+//       // 前後30秒を抽出
+//       let filtered = items.filter(item => {
+//         if(start_time - 30 < item.timestamp && item.timestamp < start_time + 30){
+//           return item
+//         }
+//       });
+//       // timestampで降順ソート
+//       filtered.sort(function(a,b){
+//         return b.timestamp-a.timestamp;
+//       });
+//       res.send(filtered);
+//     }
+//   });
+// });
+
+// 履歴表示用（前後30秒以降で揺れが落ち着くまで（/moveHistoryにmoveの判定を追加）)
+router.get('/move/history', function(req, res, next) {
   let start_time = Number(req.query.timestamp);
 
   let params = {
@@ -144,16 +173,35 @@ router.get('/moveHistory', function(req, res, next) {
       res.send(err);
     } else {
       let items = data.Items;
-      // 前後30秒を抽出
-      let filtered = items.filter(item => {
-        if(start_time - 30 < item.timestamp && item.timestamp < start_time + 30){
-          return item
-        }
-      });
-      filtered = filtered.sort(function(a,b){
+
+      // timestampで降順ソート
+      items.sort(function(a,b){
         return b.timestamp-a.timestamp;
       });
-      res.send(filtered);
+      // 対象のインデックスを取得
+      let targetIndex = items.findIndex(item=>item.timestamp == start_time);
+
+      let response = [];
+      // 時間と動いているかの閾値
+      let time_th = 30;
+      let move_th = 30;
+      // push前の履歴
+      for(var i=targetIndex;i<items.length;i++){
+        if(items[i].move < move_th && Math.abs(items[i].timestamp - items[targetIndex].timestamp) > time_th ){
+          break;
+        }
+        response.push(items[i]);
+      }
+      // push後の履歴
+      if(i>0){
+        for(var i=targetIndex-1;i>0;i--){
+          if(items[i].move < move_th && Math.abs(items[i].timestamp - items[targetIndex].timestamp) > time_th ){
+            break;
+          }
+          response.unshift(items[i]);
+        }
+      }
+      res.send(response);
     }
   });
 });
